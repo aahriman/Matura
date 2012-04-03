f3b19b1f6b8b2ee71e8a6957f5aeee1cb7fa5a4b
Vojtech Sejkora <vojtech.sejkora@seznam.cz>
2012-04-03 23:59:09 +0200

    Signed-off-by: Vojtech Sejkora <vojtech.sejkora@seznam.cz>

--- MHD/src/utilities/pomocneProgramy/UdalostProTextField.java
+++
@@ -1,6 +0,0 @@
-package utilities.pomocneProgramy;
-
-abstract public class UdalostProTextField {
-
-	abstract public void udalost();
-}
--- MHD/src/utilities/rozhrani/IPosluchac.java
+++
@@ -1,9 +0,0 @@
-package utilities.rozhrani;
-
-public interface IPosluchac<T> {
-	//== VEŘEJNÉ KONSTANTY =========================================================
-	//== DEKLAROVANÉ METODY ========================================================
-	public void zmena(T vysilac);
-	//== ZDĚDĚNÉ METODY ============================================================
-	//== INTERNÍ DATOVÉ TYPY =======================================================
-}
